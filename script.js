963ec866ec6e4d0e3c26f823051005b324d8e14c
Tony Nguyen <nguyen.tony1015@gmail.com>
2020-11-08 21:58:15 -0500

    game

--- script.js
+++ script.js
@@ -12,10 +12,36 @@ let players = [
     }
 ]
 const dealButton = document.querySelector("#deal")
+const standButton = document.querySelector("#stand")
 let playerScoreButton = document.querySelector(".player-score")
 let dealerScoreButton = document.querySelector(".dealer-score")
+const player = document.querySelector("#player");
+let banner = document.createElement("div")
+const gameContainer = document.querySelector(".game-container")
+let gameMessage = document.createElement("div")
 let playerScore = 0;
 let dealerScore = 0;
+let gameStart = false
+let dealerSecondCardImgTag = "";
+// let dealerSecondCard = document.querySelector(".dealer-second-card")
+let dcardFaceDown = document.createElement("img");
+dcardFaceDown.classList.add("dealer-second-card");
+
+// let gameMessageModal = document.querySelector("#game-message-outcome")
+
+// let modalBtn = document.querySelector(".modal-btn")
+// let modalBg = document.querySelector(".modal-bg")
+
+// modalBtn.addEventListener("click", function () {
+//     modalBg.classList.add("bg-active")
+// })
+
+// let modalClose = document.querySelector(".modal-close")
+// modalClose.addEventListener("click", function () {
+//     modalBg.classList.remove("bg-active")
+// })
+
+
 
 
 function createDeck() {
@@ -78,39 +104,162 @@ function shuffle(deck) {
     }
 }
 
+const updateScores = {
+    updateHTMLScore() {
+        playerScoreButton.innerText = playerScore;
+        // if (players[1].hand.length == 2) {
+        //     dealerScoreButton.innerText = players[1].hand[0].value
+        // } else { dealerScoreButton.innerText = dealerScore; }
+        dealerScoreButton.innerText = players[1].hand[0].value
+
+    },
+    updateCounterScore() {
+        playerScore = 0
+        dealerScore = 0
+        for (let i = 0; i < 2; i++) {
+            for (let j = 0; j < players[i].hand.length; j++) {
+                // console.log(players[i].hand[j].value)
+                if (i == 0) {
+                    playerScore += Number(players[0].hand[j].value)
+                } else if (i == 1) {
+                    dealerScore += Number(players[1].hand[j].value)
+                }
+            }
+        }
+    },
+    checkA(which) {
+        updateScores.updateCounterScore();
+        players[which].hand.sort(function (a, b) {
+            return b.value - a.value
+        })
+
+        if (which == Number(0)) {
+            if ((playerScore > 21) && (players[0].hand[0].value == Number(11))) {
+                players[0].hand[0].value = 1;
+                updateScores.updateCounterScore();
+            }
+        } else if (which == Number(1)) {
+            if ((dealerScore > 21) && (players[1].hand[0].value == Number(11))) {
+                players[1].hand[0].value = 1;
+                updateScores.updateCounterScore();
+            }
+        }
+    },
+    dealHitEndCondition() {
+        gameContainer.append(gameMessage)
+        if ((playerScore == 21) && (dealerScore == 21)) {
+            // console.log("Huh. What are the oods. Both getting 21.")
+            gameMessage.innerText = "TIE"
+            gameMessage.classList.add("game-message")
+            dealerScoreButton.innerText = dealerScore;
+            gameStart = false;
+            updateScores.flipDealerSecond();
+        } else if ((playerScore == 21) && (players[0].hand.length == 2)) {
+            // console.log("Whoa! 21 right off the bat!")
+            gameMessage.innerText = "YOU WIN"
+            gameMessage.classList.add("game-message")
+            dealerScoreButton.innerText = dealerScore;
+            gameStart = false;
+            updateScores.flipDealerSecond();
+
+        } else if (playerScore > 21) {
+            // console.log("You bust..")
+            gameMessage.innerText = "YOU LOSE"
+            gameMessage.classList.add("game-message")
+            gameStart = false;
+            updateScores.flipDealerSecond();
+            dealerScoreButton.innerText = dealerScore;
+        }
+    },
+    standEndCondition() {
+        if (playerScore == 21 && dealerScore == 21) {
+            gameMessage.innerText = "YOU WIN"
+            // gameMessage.classList.add("game-message")
+        } else if (dealerScore > 21) {
+            // console.log("T You win!")
+            gameMessage.innerText = "YOU WIN"
+            // gameMessage.classList.add("game-message")
+        } else if (playerScore == dealerScore) {
+            // console.log("You tied...")
+            gameMessage.innerText = "TIE"
+            // gameMessage.classList.add("game-message")
+        } else if (playerScore > dealerScore) {
+            // console.log("You win!")
+            gameMessage.innerText = "YOU WIN"
+            // gameMessage.classList.add("game-message")
+        } else if (playerScore < dealerScore) {
+            // console.log("You lost...")
+            gameMessage.innerText = "YOU LOSE"
+            // gameMessage.classList.add("game-message")
+        }
+        gameMessage.classList.add("game-message")
+    },
+    flipDealerSecond() {
+        // let dealerSecondCard = document.querySelector(".dealer-second-card")
+        // dealerSecondCard.src = dealerSecondCardImgTag
+        dcardFaceDown.src = dealerSecondCardImgTag
+
+        // "./deck/" + players[1].hand[1].imgTag
+    }
+}
+
+
+
+
+// -------- Deal Button -------------
 
 function deal() {
     shuffle(deck)
-    if (dealButton.innerText == "play") {
+    if (dealButton.innerText == "PLAY") {
         for (let i = 0; i < 2; i++) {
             for (let j = 0; j < players.length; j++) {
                 // do a pause?
                 let card = deck.pop();
+
                 players[j].hand.push(card);
+
             }
         }
-        updateScore();
+        let dealer = document.querySelector("#dealer");
+        let dcard = document.createElement("img");
 
-        let player = document.querySelector("#player");
+        // Imgs for player cards
         players[0].hand.forEach(function (playerCard) {
             let pcard = document.createElement("img");
             pcard.src = "./deck/" + playerCard.imgTag
             player.append(pcard)
         })
 
-        let dealer = document.querySelector("#dealer");
-        players[1].hand.forEach(function (dealerCard) {
-            let dcard = document.createElement("img");
-            dcard.src = "./deck/" + dealerCard.imgTag
-            dealer.append(dcard)
-        })
-        dealButton.innerText = "restart"
-    } else if (dealButton.innerText == "restart") {
+        // First card image for dealer
+        dcard.src = "./deck/" + players[1].hand[0].imgTag
+        dealerSecondCardImgTag = "./deck/" + players[1].hand[1].imgTag
+        dealer.append(dcard)
+        // let dcardFaceDown = document.createElement("img");
+        dcardFaceDown.src = "./deck/back_face.png"
+        // Just needs to change this. ^
+        // dcardFaceDown.classList.add("dealer-second-card")
+        dealer.append(dcardFaceDown)
+
+        // Score updater
+        updateScores.checkA(0);
+        updateScores.checkA(1);
+        updateScores.updateCounterScore();
+        updateScores.updateHTMLScore();
+        gameStart = true;
+        updateScores.dealHitEndCondition();
+
+
+        // Changes deal to restart
+        dealButton.innerText = "RESTART"
+
+    } else if (dealButton.innerText == "RESTART") {
         let playerCardImg = document.querySelectorAll("#player img")
         let dealerCardImg = document.querySelectorAll("#dealer img")
 
         for (let i = 0; i < playerCardImg.length; i++) {
             playerCardImg[i].remove();
+        }
+        for (let i = 0; i < dealerCardImg.length; i++) {
             dealerCardImg[i].remove();
         }
 
@@ -124,39 +273,75 @@ function deal() {
         dealerScoreButton.innerText = 0;
         playerScore = 0;
         dealerScore = 0;
-        dealButton.innerText = "play"
-    }
-}
+        dealButton.innerText = "PLAY"
 
-
-function updateScore() {
-    for (let i = 0; i < 2; i++) {
-        for (let j = 0; j < players[i].hand.length; j++) {
-            // console.log(players[i].hand[j].value)
-
-            if (i == 0) {
-                playerScore += Number(players[0].hand[j].value)
-            } else if (i == 1) {
-                dealerScore += Number(players[1].hand[j].value)
+        deck.forEach(function (card) {
+            if (card.face == "ace") {
+                card.value = 11
             }
-        }
-    }
-    playerScoreButton.innerText = playerScore;
-    dealerScoreButton.innerText = dealerScore;
-}
+        })
 
+        gameMessage.remove();
+        gameMessage.classList.remove("game-message");
+        gameMessage.innerText = "";
+
+        gameStart = false
+
+    }
+}
 
 dealButton.addEventListener("click", deal)
 
 
+// --------- Hit Button ----------
 function hit() {
+    if (gameStart) {
+        let card = deck.pop();
+        // let player = document.querySelector("#player");
+        let pcard = document.createElement("img");
+        players[0].hand.push(card);
+        pcard.src = "./deck/" + card.imgTag;
+        player.append(pcard)
+        updateScores.checkA(0);
+        updateScores.updateHTMLScore();
+        updateScores.dealHitEndCondition()
+    }
+}
 
+const hitButton = document.querySelector('#hit')
+hitButton.addEventListener("click", hit)
+
+// ----------- Stand Button -----------
+
+function stand() {
+    if (gameStart) {
+        dealerScoreButton.innerText = dealerScore;
+
+        updateScores.flipDealerSecond();
+
+        while (dealerScore <= 15) {
+
+            let card = deck.pop();
+            // let player = document.querySelector("#player");
+            let dcard = document.createElement("img");
+            players[1].hand.push(card);
+            dcard.src = "./deck/" + card.imgTag;
+            dealer.append(dcard)
+            updateScores.checkA(1);
+            updateScores.updateCounterScore();
+            updateScores.updateHTMLScore();
+            dealerScoreButton.innerText = dealerScore;
+        }
+        updateScores.standEndCondition();
+
+        gameStart = false
+    }
 
 }
 
+standButton.addEventListener("click", stand)
 
-
-
+// -------------------------------
 
 
 // const resetButton = document.querySelector("#reset")
@@ -178,6 +363,4 @@ function hit() {
 //     // console.log(deck.length)
 // }
 // resetButton.addEventListener("click", reset)
-
-
 
